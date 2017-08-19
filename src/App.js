2f132d0f240acc910d2a137d0561cf6e6099786c
Shelton Leung <shelton.c.leung@gmail.com>
2017-08-18 23:07:32 -0500

    Merge success

--- src/App.js
+++ src/App.js
@@ -43,23 +43,123 @@ function TabsExampleSimple(props) {
   return <Tabs>
     <Tab label="About Me">
       <div>
-        <h2 style={styles.headline}>Tab One</h2>
+        <h2 style={styles.headline}>What's your financial situation?</h2>
 
         <SlideControl name="sliderSavingAvailable"
                           name={'Spacing Available'}
                           min={0}
+                          max={100000}
+                          step={500}
+                          value={10000}
+                          prefix={'$'}/>
+
+        <SlideControl name="giftMoney"
+                          name={'Gift Money'}
+                          min={0}
                           max={40000}
                           step={500}
-                          value={2000}/>
+                          prefix={'$'}
+                          value={0}/>
+                          
+        <SlideControl name="currentMonthlyRent"
+                          name={'Current Monthly Rent'}
+                          min={0}
+                          max={10000}
+                          step={100}
+                          value={1000}
+                          prefix={'$'}/>
+
+        <SlideControl name="monthlyDisposableIncome"
+                          name={'Monthly Disposable Income'}
+                          min={0}
+                          max={15000}
+                          step={1000}
+                          value={100}
+                          prefix={'$'}/>
       </div>
     </Tab>
 
     <Tab label="My Mortgage Plan">
       <div>
-        <h2 style={styles.headline}>Tab Two</h2>
-        <p>
-          This is another example tab.
-        </p>
+        <h2 style={styles.headline}>What kind of mortgage do I want to explore?</h2>
+        
+        <SlideControl name="mortgageLength"
+                          name={'Mortgage Length'}
+                          min={5}
+                          max={30}
+                          step={5}
+                          value={30}
+                          suffix={' years'}/>
+                                  
+        <SlideControl name="percentDown"
+                          name={'Percent Down'}
+                          min={5}
+                          max={100}
+                          step={5}
+                          value={5}
+                          suffix={'%'}/>
+                                  
+        <SlideControl name="housePriceK"
+                          name={'House Price'}
+                          min={50}
+                          max={2000}
+                          step={5}
+                          value={30}
+                          suffix={' years'}/>
+      </div>
+    </Tab>
+
+    <Tab label="Assumptions">
+      <div>
+        <h2 style={styles.headline}>If you want to get more complicated and advanced, explore these dials.</h2>
+        
+        <SlideControl name="mortgageRate"
+                          name={'Mortgage Rate'}
+                          min={0.25}
+                          max={20.0}
+                          step={0.25}
+                          value={4.25}
+                          suffix={'%'}/>
+                                  
+        <SlideControl name="closingCost"
+                          name={'Closing Cost'}
+                          min={0}
+                          max={10000}
+                          step={500}
+                          value={5000}
+                          prefix={'$'}/>
+                                  
+        <SlideControl name="propertyTaxRate"
+                          name={'Property Tax Rate'}
+                          min={0.0}
+                          max={5.0}
+                          step={0.2}
+                          value={2.0}
+                          suffix={'%'}/>
+                                  
+        <SlideControl name="hoaMonthly"
+                          name={'HOA Monthly'}
+                          min={0}
+                          max={1500}
+                          step={25}
+                          value={150}
+                          prefix={'$'}/>
+                                  
+        <SlideControl name="insuranceAnnual"
+                          name={'Insurance Annual'}
+                          min={0}
+                          max={3000}
+                          step={50}
+                          value={1000}
+                          prefix={'$'}/>
+                                  
+        <SlideControl name="maintenanceAnnual"
+                          name={'maintenanceAnnual'}
+                          min={0}
+                          max={10000}
+                          step={500}
+                          value={2500}
+                          prefix={'$'}/>
       </div>
     </Tab>
 
